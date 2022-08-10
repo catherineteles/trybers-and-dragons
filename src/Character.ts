95c59393cad82ffa58beb266421d541aba928342
cathe <cacateles12@gmail.com>
2022-08-10 00:08:00 -0300

    Added SimpleFighter interface

--- src/Character.ts
+++ src/Character.ts
@@ -61,13 +61,8 @@ export default class Character implements Fighter {
   receiveDamage(attackPoints: number): number {
     const damage = this._defense - attackPoints;
     
-    if (damage > 0) {
-      this._lifePoints -= damage;
-      if (this._lifePoints <= 0) {
-        this._lifePoints = -1;
-      }
-      return this._lifePoints;
-    }
+    if (damage > 0) this._lifePoints -= damage;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
     return this._lifePoints;
   }
 
--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,13 +1,13 @@
 import Energy from '../Energy';
 
 export default interface Fighter {
-  lifePoints: number;
-  strength: number;
-  defense: number;
-  energy?: Energy;
+  lifePoints: number,
+  strength: number,
+  defense: number,
+  energy?: Energy,
 
-  attack(enemy: Fighter): void;
-  special?(enemy: Fighter): void;
-  levelUp(): void;
-  receiveDamage(attackPoints: number): number;
+  attack(enemy: Fighter): void,
+  special?(enemy: Fighter): void,
+  levelUp(): void,
+  receiveDamage(attackPoints: number): number,
 }
