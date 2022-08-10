3318b25b9be3acf905838128c530288f35cc8d75
cathe <cacateles12@gmail.com>
2022-08-10 15:15:26 -0300

    Changed order in damage to attackPoints - defense

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,3 +1,8 @@
+import Character from '../Character';
 import Battle from './Battle';
 
-export default class PVP extends Battle {}
+export default class PVP extends Battle {
+  constructor(protected player1: Character, protected player2: Character) {
+    super(player1);
+  }
+}
