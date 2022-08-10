ebe952c11f35c7577db6df9d9434dade3b9510e2
cathe <cacateles12@gmail.com>
2022-08-10 15:50:51 -0300

    Added class PVP with battle

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,8 +1,28 @@
-import Character from '../Character';
+// import Character from '../Character';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  constructor(protected player1: Character, protected player2: Character) {
+  private _player1: Fighter;
+  private _player2: Fighter;
+  constructor(protected player1: Fighter, protected player2: Fighter) {
     super(player1);
+    this._player1 = player1;
+    this._player2 = player2;
+  }
+
+  isBattleOver(): boolean {
+    if (this.player1.lifePoints === -1 || this.player2.lifePoints === -1) {
+      return true;
+    }
+    return false;
+  }
+
+  fight(): number {
+    while (!this.isBattleOver()) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+    return this.player1.lifePoints === -1 ? -1 : 1;
   }
 }
