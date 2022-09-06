f90012bf88a638951b8f2c9d8d1d2e25736c1d6d
cathe <cacateles12@gmail.com>
2022-09-06 18:39:49 -0300

    Desenvolvendo PVE incompleto

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -2,28 +2,26 @@ import Fighter, { SimpleFighter } from '../Fighter';
 import Battle from './Battle';
 
 export default class PVE extends Battle {
-  private _player: Fighter;
-  private _monsters: SimpleFighter[];
-  constructor(player: Fighter, monsters: SimpleFighter[]) {
+  constructor(player: Fighter, private monsters: SimpleFighter[]) {
     super(player);
-    this._player = player;
-    this._monsters = monsters;
   }
 
-  // isBattleOver(monster: SimpleFighter): boolean {
-  //   if (this._player.lifePoints === -1 || monster.lifePoints === -1) {
+  // isBattleOver(): boolean {
+  //   if (this.player.lifePoints === -1 || this.monsters
+  //     .some((monster) => monster.lifePoints === -1)) {
   //     return true;
   //   }
   //   return false;
   // }
 
   // fight(): number {
-  //   this._monsters.forEach((monster) => {
-  //     while (!this.isBattleOver(monster)) {
-  //       this._player.attack(monster);
+  //   while (this.player.lifePoints !== -1 || this.monsters
+  //     .every((monster) => monster.lifePoints !== -1)) {
+  //     this.monsters.forEach((monster) => {
+  //       this.player.attack(monster);
   //       monster.attack(this.player);
-  //     }
-  //   });
-  //   return this._player.lifePoints === -1 ? -1 : 1;
+  //     });
+  //   }
+  //   return this.player.lifePoints === -1 ? -1 : 1;
   // }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import Character from './Character';
-import Dragon from './Dragon';
-import Monster from './Monster';
+// import Character from './Character';
+// import Dragon from './Dragon';
+// import Monster from './Monster';
 
-const player1 = new Character('Kvoth');
-const player2 = new Character('Doctor');
-const player3 = new Character('Shadow');
+// const player1 = new Character('Kvoth');
+// const player2 = new Character('Doctor');
+// const player3 = new Character('Shadow');
 
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
+// player1.levelUp();
+// player1.levelUp();
+// player1.levelUp();
 
-const monster1 = new Monster();
-const monster2 = new Dragon();
+// const monster1 = new Monster();
+// const monster2 = new Dragon();
 
-export { player1, player2, player3, monster1, monster2 };
+// export { player1, player2, player3, monster1, monster2 };
